045cb8bfc1b14f1f5bedf76fec4be9afdf0f7524
sharkpb8 <sharkpb8@gmail.com>
2024-01-10 09:12:36 +0100

    added more pictures and painters

--- script.js
+++ script.js
@@ -50,11 +50,16 @@ class Picture {
         if (this.author === undefined) {
             return `Painting: ${this.name}`;
         }
-        return `Painting: ${this.name}; Price: ${this.price}; Year of creation: ${this.year}; Author: ${this.author.getAuthor()};`;
+        return `Painting: ${this.name}; Price: ${this.price}milion; Year of creation: ${this.year}; Author: ${this.author.getAuthor()};`;
     }
 }
 
 let davinci = new Painter("Leonardo", "Vinci", 1452);
+let diCaprio = new Painter("Leonardo", "DiCaprio", 1974);
+let adolf = new Painter("Adolf", "Hitler", 1889);
+
 let mona = new Picture(davinci, "MonaLisa", 860, 1503);
+let titanic = new Picture(diCaprio,"Titanic",200,1998);
+let lakehouse = new Picture(adolf, "House at a lake with mountains",5,1910);
 console.log(davinci);
 console.log(mona.getPainting());
